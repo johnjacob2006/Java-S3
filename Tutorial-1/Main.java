5093585ca6d9db2a893624a543988c6fe6339b87
John Jacob <johnjacobvk@gmail.com>
2026-08-12 23:06:05 +0530

    Add Main class with data type demonstrations
    This Java program demonstrates the use of various data types including byte, short, int, long, float, double, char, and boolean. It prints the values of these data types to the console.

---
+++ Tutorial-1/Main.java
@@ -0,0 +1,24 @@
+class Main {
+    public static void main(String[] args) {
+
+        byte a = 10;
+        short b = 1000;
+        int c = 50000;
+        long d = 100000L;
+
+        float e = 10.5f;
+        double f = 20.5678;
+
+        char g = 'A';
+        boolean h = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
